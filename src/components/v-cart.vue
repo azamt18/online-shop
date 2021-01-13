564056bc2646151cbef20619caf079ce2c27b61a
Azam <azam.turgunboyev@gmail.com>
2021-01-13 18:11:07 +0500

    Initial commit - done tasks up to getting data from API

---
+++ src/components/v-cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-cart">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-cart",
+  props: {},
+  data() {
+    return {
+      title: 'Cart'
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="v-main-wrapper">
+    <v-catalog />
+    <v-cart />
+  </div>
+</template>
+
+<script>
+  import vCatalog from './v-catalog'
+  import vCart from './v-cart'
+
+  export default {
+    name: 'v-main-wrapper',
+    components: {
+      vCatalog,
+      vCart
+    },
+    data() {
+      return {
+        title: 'Main wrapper'
+      }
+    },
+    computed: { },
+    methods: { },
+    watch: { },
+    mounted() {
+      console.log('Hello from console to Main wrapper')
+    }
+  }
+</script>
+
+<style scoped>
+  .v-main-wrapper {
+    max-width: 900px;
+    margin: 0 auto;
+  }
+</style>
