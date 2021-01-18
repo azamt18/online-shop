465f9768904e6b5e54600cea743af47a713331f8
Azam <azam.turgunboyev@gmail.com>
2021-01-18 21:24:27 +0500

    Show item details in SLOT and close it using $refs

--- src/components/catalog/v-catalog-item.vue
+++ src/components/catalog/v-catalog-item.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="v-catalog-item">
+
+    <v-popup
+      v-if="isInfoPopupVisible"
+      @closePopup="closePopup"
+      @rightBtnAction="addToCart"
+      :popupTitle="product_data.name"
+      rightBtnTitle="Add to cart"
+    >
+      <img class="v-catalog-item__image" :src=" require('../../assets/images/' + product_data.image) " alt="img">
+      <div>
+        <p class="v-catalog-item__name">{{ product_data.name }}</p>
+        <p class="v-catalog-item__price">Price: {{ product_data.price }}</p>
+        <p class="v-catalog-item__price">Price: {{ product_data.category }}</p>
+      </div>
+    </v-popup>
+
     <img class="v-catalog-item__image" :src=" require('../../assets/images/' + product_data.image) " alt="img">
     <p class="v-catalog-item__name">{{ product_data.name }}</p>
     <p class="v-catalog-item__price">Price: {{ product_data.price }}</p>
+    <button
+      class="v-catalog-item__show_info"
+      @click="showPopupInfo"
+    >
+      Show info
+    </button>
     <button class="v-catalog-item__add_to_cart_btn btn" @click="addToCart">Add to cart</button>
   </div>
 </template>
 
 <script>
+import vPopup from '../popup/v-popup'
+
 export default {
   name: "v-catalog-item",
+  components: {
+    vPopup
+  },
   props: {
     product_data: {
       type: Object,
@@ -19,10 +46,18 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      isInfoPopupVisible: false
+    }
   },
   computed: {},
   methods: {
+    closePopup() {
+      this.isInfoPopupVisible = false;
+    },
+    showPopupInfo() {
+      this.isInfoPopupVisible = true;
+    },
     sendDataToParent() {
       this.$emit('sendDataToParent', this.product_data.article);
     },
